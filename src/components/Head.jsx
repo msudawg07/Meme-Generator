8ba0566a6e8ad364b1c81a70b7a629681ac11333
Austin <austin.fratesi@gmail.com>
2025-06-24 21:45:49 -0500

    initial commit

---
+++ src/components/Head.jsx
@@ -0,0 +1,7 @@
+import meme from '../assets/meme.png'
+
+export default function Head() {
+  return (
+    <div className="header"><img src={meme}/> <span>Meme Generator</span></div>
+  )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+export default function Main() {
+  return (
+    <main>
+
+      <div className="formContainer">
+        <div>
+          <label>Top Text</label>
+          <input type="text"></input>
+        </div>
+
+        <div>
+          <label>Bottom Text</label>
+          <input type="text"></input>
+        </div>
+      </div>
+
+      <button className="getNewImageButton">Get a new meme image 🖼</button>
+
+    </main>
+  )
+}
