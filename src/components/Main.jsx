4d53e7d0622571a7e6db36cf2e5273df09a99bab
Austin <austin.fratesi@gmail.com>
2025-06-24 22:17:11 -0500

    latest update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,6 +16,12 @@ export default function Main() {
 
       <button className="getNewImageButton">Get a new meme image 🖼</button>
 
+      <div className="meme">
+        <div className="topText">Top writiogt</div>
+        <img src="https://wallpapers.com/images/featured/game-of-thrones-pictures-ad9d02jxoawpkpqg.jpg" className="memeImage"></img>
+        <div className="bottomText">Bottom writiogt</div>
+      </div>
+
     </main>
   )
 }
